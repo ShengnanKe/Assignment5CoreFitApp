b908a88cdb6a77c83678c2bc6b834ad8698ca81f
KknanxX <72762570+KknanxX@users.noreply.github.com>
2024-05-21 17:08:06 -0700

    Initial Commit

---
+++ Assignment5CoreFitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment5CoreFitApp
+//
+//  Created by KKNANXX on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
